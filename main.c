fc855e296150495aa15c69e3742ea163dc119df2
vlad-PC\vlad <emevladstefan95@gmail.com>
2017-05-24 22:29:46 +0300

    project skeleton

--- main.c
+++ main.c
@@ -9,9 +9,85 @@
 // deoarece pe y ecranul incepe de la pixelul 33, avem nevoie de un offset
 #define OFFSET _height - _width 
 
+//draw START 
+int snake[80];
+typedef struct {
+    uint8_t x;
+    uint8_t y;
+} coord;
+coord snake_coord[80];
+int direction;
+short x1Obst1 = 10;
+short y1Obst1 = 10;
+short x2Obst1 = 100;
+short y2Obst1 = 10;
+
+short x1Obst2 = 60;
+short y1Obst2 = 10;
+short x2Obst2 = 60;
+short y2Obst2 = 60;
+
+short x1Obst3 = 110;
+short y1Obst3 = 20;
+short x2Obst3 = 110;
+short y2Obst3 = 110;
+
+
+void initGame(){
+	int i;
+	for(i = 60; i < 80; i++ )
+		snake[i] = 1;
+	for(i = 0; i < 60; i++ )
+		snake[i] = 0;
+	for(i = 0; i < 10; i++ )
+		direction = 0;
+	for(i = 0; i < 80; i++ )
+		if(snake[i] == 1){
+			snake_coord[i].x = i;
+			snake_coord[i].y = 30;
+		}
+}
+
+void draw_snake() {
+	int i;
+	for(i = 0; i < 80; i++ )
+		if(snake[i] == 1){
+			LCD_draw_pixel(snake_coord[i].x, snake_coord[i].y, 64);
+		}
+}
+
+void update_snake(){
+//right
+	int i;
+	if(direction == 1){
+		for(i = 80; i > 0; i-- )
+			if(snake[i] == 1) {
+				snake_coord[i].x = snake_coord[i-1].x;
+				snake_coord[i].y = snake_coord[i-1].y;
+			}
+		snake_coord[i].x += 1; 
+	}
+}
+
+
+void drawObstacles() {
+	GFX_draw_line(10, 10, 100, 10, 64);
+	GFX_draw_line(60, 10, 60, 60, 64);
+	GFX_draw_line(110, 20, 110, 110, 64);
+}
+
+void gameStart(){
+	GFX_draw_line(10, 10, 30, 10, 64);
+	GFX_draw_line(10, 10, 10, 30, 64);
+	GFX_draw_line(10, 30, 30, 30, 64);
+	GFX_draw_line(30, 30, 30, 50, 64);
+	GFX_draw_line(30, 50, 10, 50, 64);
+}
+
 int main(void){
     // init the lcd display
     LCD_init();
+	initGame();
 	
 	LCD_fill_screen(126);
 	//GFX_draw_line(100, 100, 120, 120, 64);
@@ -20,14 +96,6 @@ int main(void){
     PORTA |= (1 << PA7);
 
     while(1){
-
-		//LCD_fill_screen(126);
-		/*LCD_draw_pixel(10, 10, 64);
-		LCD_draw_pixel(11, 10, 64);
-		LCD_draw_pixel(12, 10, 64);
-		LCD_draw_pixel(13, 10, 64);
-		LCD_draw_pixel(14, 10, 64);
-		LCD_draw_pixel(15, 10, 64);*/
 		
 		while((PINA & (1 << PA7)) != 0);
 
@@ -38,7 +106,6 @@ int main(void){
         }
 
 		_delay_ms(10);
-		//GFX_draw_line(10, 10, 20, 20, 64);
     }
 
     return 0;
